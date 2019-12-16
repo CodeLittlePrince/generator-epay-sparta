bdcf9714b6a8add6fc73726c7738e06f99a1c93e
倪俊杰 <1006312908@qq.com>
2019-12-16 13:44:43 +0800

    升级koa-mock-switch

--- generators/app/templates/mockServer/index.js
+++ generators/app/templates/mockServer/index.js
@@ -2,23 +2,18 @@ const devConfig = require('../webpack/dev.config')
 const path = require('path')
 // mock文件的根目录
 const mockRoot = path.join(__dirname, '../mock')
-// require koa-mock-switch
 const KoaMockSwitch = require('koa-mock-switch')
 // mock管理列表
 const mockSwitchMap = require('./mockSwitchMap')
-/**
- * KoaMockSwitch(mockRoot, mockSwitchMap, apiSuffix)
- * @param mockRoot mock文件的根目录
- * @param mockSwitchMap mock管理列表
- * @param apiSuffix 客户端请求api的后缀，比如'/api/kitty.json'，apiSuffix就是'.json'
- */
 const mock = new KoaMockSwitch({
   root: mockRoot,
+  port: devConfig.proxyServerPort,
   switchMap: mockSwitchMap,
+  apiPrefix: '',
   apiSuffix: '.json'
 })
 // 启动mock服务
-mock.start(devConfig.proxyServerPort)
+mock.start()
 // nodemon会command+c终止终端都无法关闭mock进程
 // 所以需要进程接受信号来调用koa-mock-switch接口来关闭
 ;['SIGINT', 'SIGTERM'].forEach(signal => {
